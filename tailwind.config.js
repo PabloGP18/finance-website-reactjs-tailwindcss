73e0028d029a50a63f968dd0d80dbf63ea9b75db
Pablo GP <garciaplaza.pablo@gmail.com>
2023-05-20 01:54:29 +0200

    Added settings for landscape view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        'mobile-landscape': {
+          raw: '(max-height: 390px) and (orientation: landscape)'
+        },
+        'tabled-landscape': {
+          raw: '(max-height: 820px) and (orientation: landscape)'
+        }
+      },
+      spacing: {
+        '160vh': '160vh',
+        '115vh': '115vh'
+      }
+    }
   },
+  variants: {},
   plugins: []
 }
